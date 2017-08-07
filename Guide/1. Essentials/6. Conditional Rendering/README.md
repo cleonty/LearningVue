0eb0ed96fa6fe6d2dd1ac45bc113193309cb85fb
Leonty Chudinov <leonty@inbox.ru>
2017-08-07 11:08:00 +0500

    Added Conditional Rendering

---
+++ Guide/1. Essentials/6. Conditional Rendering/README.md
@@ -0,0 +1,1 @@
+# [Conditional Rendering](https://vuejs.org/v2/guide/conditional.html)
---
+++ Guide/1. Essentials/6. Conditional Rendering/app.js
@@ -0,0 +1,10 @@
+/* global Vue */
+(function () {
+  var vm = new Vue({
+    el: '#ConditionalRendering',
+    data: {
+      ok: true
+    }
+  });
+})();
+
